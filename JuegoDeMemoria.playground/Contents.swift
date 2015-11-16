a083343f996a43ea066991e2de0bfa5f2d31b308
Francisco Parra <chipita@chipitas-MacBook-Pro.local>
2015-11-15 23:39:09 -0600

    juego de memoria

---
+++ JuegoDeMemoria.playground/Contents.swift
@@ -0,0 +1,24 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+
+for i in 0...100
+{
+    if (i % 5)==0{
+        print("\(i) el número Bingo!!!")
+    }
+    if (i % 2)==0{
+        print("\(i) el número par!!!")
+    }
+    if (i % 2)==1{
+        print("\(i) el número impar!!!")
+    }
+    
+    if (i>=30) && (i<=40){
+        print("\(i) el número Viva Swift!!!")
+    }
+    
+}
+
